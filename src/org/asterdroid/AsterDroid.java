fa28040bbb2470abcddd34468c5bef24e8166bcb
Francois Deppierraz <francois@ctrlaltdel.ch>
2009-06-28 03:39:16 +0200

    Moved XMPP stuff into a the Notificator service

--- src/org/asterdroid/AsterDroid.java
+++ src/org/asterdroid/AsterDroid.java
@@ -3,30 +3,14 @@ package org.asterdroid;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.jivesoftware.smack.ConnectionConfiguration;
-import org.jivesoftware.smack.PacketListener;
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.filter.MessageTypeFilter;
-import org.jivesoftware.smack.filter.PacketFilter;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Packet;
-import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.util.StringUtils;
-
-
 public class AsterDroid extends Activity {
 	TextView tv;
 	Context context;
-    XMPPConnection connection;
 	
     /** Called when the activity is first created. */
     @Override
@@ -47,99 +31,11 @@ public class AsterDroid extends Activity {
             }
         });
         
-        try {
-        	XMPPSetup();
-        } catch (Exception e) {
-            Intent myIntent = new Intent();
-            myIntent.setClassName("org.asterdroid", "org.asterdroid.Settings");
-            startActivity(myIntent);
-        }
-    }
-    
-    public void XMPPSetup() {
-    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-    	
-    	String host = prefs.getString("hostname", "");
-    	int port = Integer.parseInt(prefs.getString("port", "5222"));
-    	String username = prefs.getString("username", "");
-    	String password = prefs.getString("password", "");
-
-        // Create a connection
-        ConnectionConfiguration connConfig =
-            new ConnectionConfiguration(host, port);
-        connection = new XMPPConnection(connConfig);
-
-        try {
-            connection.connect();
-            Log.i("XMPPClient", "Connected to " + connection.getHost());
-        } catch (XMPPException ex) {
-            Log.e("XMPPClient", "Failed to connect to " + connection.getHost());
-            Log.e("XMPPClient", ex.toString());
-            connection = null;
-        }
-        try {
-            connection.login(username, password);
-            Log.i("XMPPClient", "Logged in as " + connection.getUser());
-
-            // Set the status to available
-            Presence presence = new Presence(Presence.Type.available);
-            connection.sendPacket(presence);
-        } catch (XMPPException ex) {
-            Log.e("XMPPClient", "Failed to log in as " + username);
-            Log.e("XMPPClient", ex.toString());
-            connection = null;
-        }
-        
-        
-        // Set a listener to send a chat text message
-        Button send = (Button) this.findViewById(R.id.Button01);
-        send.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                String text = "Salut depuis Android";
-                String to = "francois@planete.ctrlaltdel.ch";
-
-                Log.i("XMPPClient", "Sending text [" + text + "] to [" + to + "]");
-                Message msg = new Message(to, Message.Type.chat);
-                msg.setBody(text);
-                connection.sendPacket(msg);
-            }
-        });
-        
-        /* Display incoming messages */
-        if (connection != null) {
-            // Add a packet listener to get messages sent to us
-            PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
-            connection.addPacketListener(new PacketListener() {
-                public void processPacket(Packet packet) {
-                    Message message = (Message) packet;
-                    if (message.getBody() != null) {
-                        String fromName = StringUtils.parseBareAddress(message.getFrom());
-                        Log.i("XMPPClient", "Got text [" + message.getBody() + "] from [" + fromName + "]");
-                        
-                        /*
-                        Toast.makeText(context, "SALUT", Toast.LENGTH_LONG).show();
-                        */
-                        
-                        Intent myIntent = new Intent();
-                        myIntent.setClassName("org.asterdroid", "org.asterdroid.IncomingCall");
-                        myIntent.putExtra("callerid", message.getBody());
-                        startActivity(myIntent);
-                        
-                        /*
-                        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:" + message.getBody()));
-                        startActivity(myIntent);
-                        */
-                    }
-                }
-            }, filter);
-        }
+        startService(new Intent().setClassName("org.asterdroid", "org.asterdroid.Notificator"));
     }
     
     public void onDestroy() {
     	super.onDestroy();
-    	
-    	if (connection != null) {
-    		connection.disconnect();
-    	}
+    	/* stopService(new Intent().setClassName("org.asterdroid", "org.asterdroid.Notificator")); */
     }
 }
--- src/org/asterdroid/Notificator.java
+++ src/org/asterdroid/Notificator.java
@@ -1,10 +1,26 @@
 package org.asterdroid;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.MessageTypeFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.util.StringUtils;
+
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.os.Vibrator;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class Notificator extends Service {
+    XMPPConnection connection;
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -15,6 +31,80 @@ public class Notificator extends Service {
 	
 	public void onCreate() {
 		super.onCreate();
+		
+	    try {
+	    	XMPPSetup();
+	    } catch (Exception e) {
+	    	System.err.println("XMPPSetup failed");
+	    }
 	}
+	
+    public void XMPPSetup() {
+    	SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    	
+    	String host = prefs.getString("hostname", "");
+    	int port = Integer.parseInt(prefs.getString("port", "5222"));
+    	String username = prefs.getString("username", "");
+    	String password = prefs.getString("password", "");
 
+        // Create a connection
+        ConnectionConfiguration connConfig =
+            new ConnectionConfiguration(host, port);
+        connection = new XMPPConnection(connConfig);
+
+        try {
+            connection.connect();
+            Log.i("XMPPClient", "Connected to " + connection.getHost());
+        } catch (XMPPException ex) {
+            Log.e("XMPPClient", "Failed to connect to " + connection.getHost());
+            Log.e("XMPPClient", ex.toString());
+            connection = null;
+        }
+        try {
+            connection.login(username, password);
+            Log.i("XMPPClient", "Logged in as " + connection.getUser());
+
+            // Set the status to available
+            Presence presence = new Presence(Presence.Type.available);
+            connection.sendPacket(presence);
+        } catch (XMPPException ex) {
+            Log.e("XMPPClient", "Failed to log in as " + username);
+            Log.e("XMPPClient", ex.toString());
+            connection = null;
+        }
+        
+        /* Display incoming messages */
+        if (connection != null) {
+            // Add a packet listener to get messages sent to us
+            PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
+            connection.addPacketListener(new PacketListener() {
+                public void processPacket(Packet packet) {
+                    Message message = (Message) packet;
+                    if (message.getBody() != null) {
+                        String fromName = StringUtils.parseBareAddress(message.getFrom());
+                        Log.i("XMPPClient", "Got text [" + message.getBody() + "] from [" + fromName + "]");
+                        
+                        Intent myIntent = new Intent();
+                        myIntent.setClassName("org.asterdroid", "org.asterdroid.IncomingCall");
+                        myIntent.putExtra("callerid", message.getBody());
+                        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        
+                        Log.i("XMPPClient", "Intent is built: " + myIntent);
+                        
+                        startActivity(myIntent);
+                                                
+                        Log.i("Incoming_call", "Activity should have been started");
+                    }
+                }
+            }, filter);
+        }
+    }
+
+    public void onDestroy() {
+    	super.onDestroy();
+   
+    	if (connection != null) {
+    		connection.disconnect();
+    	}
+    }
 }
